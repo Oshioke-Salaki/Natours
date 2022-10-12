f6b9c5b80fb43157d1a41a7f77b9b3aa8f2ad36b
Oshioke-Salaki <salaki1902@gmail.com>
2022-10-11 20:17:00 -0600

    Implemented password Hashing in user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+//bcrypt is used for hashing passwords on save
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -25,15 +27,39 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, 'Please confirm your password'],
+        validate: {
+            //the validator key has a function that either returns true or false
+            //this refers to the mongoose schema object
+            //this only works on create or save
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same',
+        },
     },
-    role: {
-        type: String,
-        required: [true, 'This user has to have a role'],
-    },
-    active: {
-        type: Boolean,
-        required: true,
-    },
+    // role: {
+    //     type: String,
+    //     required: [true, 'This user has to have a role'],
+    // },
+    // active: {
+    //     type: Boolean,
+    //     required: true,
+    // },
+});
+
+userSchema.pre('save', async function(next) {
+    //We only want to run this function if the password has been modified
+    if (!this.isModified('password')) return next();
+
+    //the second parameter in the hash method is the cost parameter. The defualt is 10. A higher number IS A MEASURE of how CPU intensive the operation will be and the better the password will be encrypted
+    //this is the asynchrounous version and it returns a promise.
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //We dont need confirm password to be persisted to the database so we set it to undefined
+    this.passwordConfirm = undefined;
+
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
