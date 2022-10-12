086d25164a94763c094cbd3ed15c0968f3f707d7
Oshioke-Salaki <salaki1902@gmail.com>
2022-10-11 20:18:48 -0600

    Implmented delete and get user controllers

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,35 +1,41 @@
+const User = require('../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 //status(500) - Internal server error
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "This route has not yet benn implemented"
-    })
-}
+exports.getAllUsers = catchAsync(async(req, res, next) => {
+    const users = await User.find();
+    await res.status(200).json({
+        status: 'success',
+        data: {
+            users,
+        },
+    });
+});
 
 exports.createUser = (req, res) => {
     res.status(500).json({
-        status: "error",
-        message: "This route has not yet benn implemented"
-    })
-}
+        status: 'error',
+        message: 'This route has not yet benn implemented',
+    });
+};
 
 exports.getUser = (req, res) => {
     res.status(500).json({
-        status: "error",
-        message: "This route has not yet benn implemented"
-    })
-}
+        status: 'error',
+        message: 'This route has not yet benn implemented',
+    });
+};
 
 exports.UpdateUser = (req, res) => {
     res.status(500).json({
-        status: "error",
-        message: "This route has not yet benn implemented"
-    })
-}
+        status: 'error',
+        message: 'This route has not yet benn implemented',
+    });
+};
 
-exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "deleted user"
-    })
-}
+exports.deleteUser = async(req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+        status: 'error',
+        message: 'deleted user',
+    });
+};
