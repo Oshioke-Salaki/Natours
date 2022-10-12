e75c7bec2235dc22f6a1a693e03dc7759c87120f
Oshioke-Salaki <salaki1902@gmail.com>
2022-10-12 13:36:03 -0600

    Created login and signup routes

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -5,6 +5,7 @@ const authController = require('./../Controllers/authController');
 const router = express.Router();
 
 router.route('/signup').post(authController.signup);
+router.post('/login', authController.login);
 
 router
     .route('/')
