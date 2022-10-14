d332170d52e0022e6fd469645bc5e1d6f6fc3538
Oshioke-Salaki <salaki1902@gmail.com>
2022-10-14 14:04:06 -0600

    updated auth controller

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -4,13 +4,17 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
+const signToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
+
 exports.signup = catchAsync(async(req, res, next) => {
     const newUser = await User.create(req.body);
 
     //Create the payload
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
-    });
+    const token = signToken(newUser._id);
     //Alternative
     // User.create({
     //     name: req.body.name,
@@ -30,21 +34,32 @@ exports.signup = catchAsync(async(req, res, next) => {
 
 exports.login = catchAsync(async(req, res, next) => {
     const { email, password } = req.body;
-    //Check if email and pasword actually exist
+    ///////////////////////////////////////////////
+    //1. Check if email and pasword actually exist
+    //////////////////////////////////////////////
     if (!email || !password) {
         return next(new AppError('Please provide email and password', 400));
     }
-    //check if user exist and if password is correct
-    // here we find a document where email is equal to the email passed in the body. email: email
 
+    /////////////////////////////////////////////////
+    //2. check if user exist and if password is correct
+    /////////////////////////////////////////////////
+    // here we find a document where email is equal to the email passed in the body. email: email
     //The .select('+password') is to include the password field in the output because it was originally not supposed to be outputted
     const user = await User.findOne({ email }).select('+password');
 
-    console.log(user);
+    // const correct = await user.correctPassword(password, user.password);
 
-    //check if everything is ok, send token to client
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        //Error 401: Unauthorized access
+        return next(new AppError('Incorrect email or password', 401));
+    }
 
-    const token = '';
+    ////////////////////////////////////////////////////
+    //3. check if everything is ok, send token to client
+    /////////////////////////////////////////////////////
+
+    const token = signToken(user._id);
     res.status(200).json({
         status: 'success',
         token,
