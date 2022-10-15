69bc153af1422c788b038c3d387e8fc12e10899f
Oshioke-Salaki <salaki1902@gmail.com>
2022-10-15 09:56:09 -0600

    Updated the auth controller

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -4,6 +4,7 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
+//This function creates a jwt token
 const signToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN,
