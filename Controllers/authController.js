ce1974604e0a7a296557ada27a74db9bc6f55f4d
Oshioke-Salaki <salaki1902@gmail.com>
2022-10-17 11:23:37 -0600

    created the restrictTo controller

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -109,3 +109,17 @@ exports.protect = catchAsync(async(req, res, next) => {
     req.user = currentUser;
     next();
 });
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        //roles is an array ['admin', 'lead-guide'], roles= user
+
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError('You do not have permission to perform this action', 403)
+            );
+        }
+
+        next();
+    };
+};
