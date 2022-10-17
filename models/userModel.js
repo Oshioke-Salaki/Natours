99b2d3f4f60ab68e8b98d2c36687367d619d597d
Oshioke-Salaki <salaki1902@gmail.com>
2022-10-17 09:38:17 -0600

    updated the user model

--- models/userModel.js
+++ models/userModel.js
@@ -65,7 +65,7 @@ userSchema.methods.correctPassword = async function(
     return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-userSchema.methods.changedPasswordAfter = async function(JWTTimestamp) {
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
     if (this.passwordChangedAt) {
         const changedTimeStamp = parseInt(
             this.passwordChangedAt.getTime() / 1000,
@@ -74,6 +74,8 @@ userSchema.methods.changedPasswordAfter = async function(JWTTimestamp) {
         console.log(changedTimeStamp, JWTTimestamp);
         return JWTTimestamp < changedTimeStamp;
     }
+
+    //False means not changed
     return false;
 };
 
