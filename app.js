9fba251d640077c41192b632efdb5b943fe116d9
Oshioke-Salaki <salaki1902@gmail.com>
2022-10-21 14:28:26 -0600

    Implemented rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //import core modules
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./Controllers/errorController');
 const tourRouter = require('./Routes/tourRoutes');
@@ -9,12 +10,22 @@ const userRouter = require('./Routes/userRoutes');
 const app = express();
 console.log(process.env.NODE_ENV);
 
-//Middlewares (Used for all routes)
+//Global Middlewares (Used for all routes)
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 } else {
     app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, Please tryagain in an hour!',
+});
+
+//this applies to all our routes that start with '/api'
+app.use('/api', limiter);
+
 app.use(express.json());
 //middleware to load static files in our browser
 app.use(express.static(`${__dirname}/public`));
